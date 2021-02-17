a1e1f2e490043e31ac59fbffd35e845af9d98ebb
Al <theonlypurplecat@gmail.com>
2021-02-17 15:17:13 +1300

    A project and some shitty movement

---
+++ Assets/Scripts/BroomMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroomMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float Speed;
+
+    //All KeyCodes
+    [SerializeField]
+    private KeyCode GoForward;
+    [SerializeField]
+    private KeyCode Left;
+    [SerializeField]
+    private KeyCode Right;
+    [SerializeField]
+    private KeyCode Up;
+    [SerializeField]
+    private KeyCode Down;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var side = 0;       
+        var vert = 0;
+        var forward = 0;
+
+
+        //moving on x
+        if (Input.GetKey(Left))
+        {
+            side -= 1;
+        }
+        if (Input.GetKey(Right))
+        {
+            side += 1;
+        }
+
+
+        //moving on y
+        if (Input.GetKey(Up))
+        {
+            vert += 1;
+        }
+        if (Input.GetKey(Down))
+        {
+            vert -= 1;
+        }
+
+
+
+        //speed additons, and movement on z
+        if (Input.GetKey(GoForward))
+        {
+            forward += 1;
+        }
+
+        var movement = new Vector3(side, vert, forward).normalized * Speed * Time.deltaTime;
+        transform.position += movement;
+
+    }
+
+    private void addSpeed()
+    {
+
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/MenuMain.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMain : MonoBehaviour
+{
+
+    // in menu pages/changes:
+    [SerializeField]
+    private GameObject Menu = null;
+    [SerializeField]
+    private GameObject CreditMenu = null;
+    [SerializeField]
+    private GameObject QuitPrompt = null;
+
+
+    //Start the Game things:
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //Settings Menus:
+    //turn Settings menu on
+    public void CreditsOn()
+    {
+        CreditMenu.SetActive(true);
+        Menu.SetActive(false);
+    }
+    //turn settings menu off
+    public void CreditsOff()
+    {
+        CreditMenu.SetActive(false);
+        Menu.SetActive(true);
+    }
+
+
+
+    //Quit things:
+
+    //when selecting the quit button bring up the Quit Yes/no option
+    public void QuitPromptOn()
+    {
+        QuitPrompt.SetActive(true);
+        Menu.SetActive(false);
+    }
+
+    //selecting No and closing the quit prompt
+    public void QuitPromptOff()
+    {
+        QuitPrompt.SetActive(false);
+        Menu.SetActive(true);
+    }
+
+    //selecting yes and quitting the game
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+
+}
