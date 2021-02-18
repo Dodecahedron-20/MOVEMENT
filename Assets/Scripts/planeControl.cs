86f72d27a773e9ac7af30ce71126c5fb1b9c3494
Al <theonlypurplecat@gmail.com>
2021-02-18 15:25:21 +1300

    working plane

--- Assets/Scripts/planeControl.cs
+++ Assets/Scripts/planeControl.cs
@@ -6,34 +6,72 @@ public class planeControl : MonoBehaviour
 {
     [SerializeField]
     private float speed = 6;
+    [SerializeField]
+    private float ForwardSpeed = 1;
 
     private Vector3 position = new Vector3(0, 1, 0);
 
+    [SerializeField]
+    private float banking = 20;
+    [SerializeField]
+    private float rise = 20;
 
 
+    [SerializeField]
+    private float RotSmoothTime = 0.1f;
+    private float RotSmoothVelocityZ;
+    private float RotSmoothVelocityX;
 
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        
 
-        //var vert = 0;
-        //var horiz = 0;
+        var vert = 0;
+        var horiz = 0;
+        var go = ForwardSpeed;
+
+        var RotX = 0f;
+        var RotY = 0f;
+        var RotZ = 0f;
 
 
-        var movement = new Vector3(horizontal, vertical, 0) * speed * Time.deltaTime;
+        //horizontal movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            horiz -= 1;
+            RotZ = -banking;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            horiz += 1;
+            RotZ = banking;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            vert += 1;
+            RotX = -rise;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            vert -= 2;
+            RotX = rise;
+        }
+
+
+        var movement = new Vector3(horiz, vert, go) * speed * Time.deltaTime;
         transform.position += movement;
 
+        float SmoothRotZ = Mathf.SmoothDampAngle(transform.eulerAngles.z, RotZ, ref RotSmoothVelocityZ, RotSmoothTime);
+        float SmoothRotX = Mathf.SmoothDampAngle(transform.eulerAngles.x, RotX, ref RotSmoothVelocityX, RotSmoothTime);
 
 
+
+        transform.rotation = Quaternion.Euler(SmoothRotX, RotY, SmoothRotZ);
     }
 }
